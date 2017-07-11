7580f4ec3f11008fe0f494a70975fa6f1ec90b83
Davide Zordan <davide.zordan@microsoft.com>
2017-07-11 21:24:38 +0100

    Adding 2D3D sample

---
+++ MixedReality2D3D/Assets/Scripts/TapBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class TapBehaviour : MonoBehaviour {
+
+    GestureRecognizer _gr;
+
+	// Use this for initialization
+	void Start () {
+        _gr = new GestureRecognizer();
+        _gr.TappedEvent += _gr_TappedEvent;
+        _gr.StartCapturingGestures();
+	}
+
+    private async void _gr_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        var contentPage = AppViewManager.Views["ContentPage"];
+        await contentPage?.SwitchAsync();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ MixedReality2D3D/UWP/MixedReality2D3D/ContentPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MixedReality2D3D
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ContentPage : Page
+    {
+        int MainAppViewId;
+
+        public ContentPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var contentPage = AppViewManager.Views["MainPage"];
+            await contentPage?.SwitchAsync();
+        }
+    }
+}
