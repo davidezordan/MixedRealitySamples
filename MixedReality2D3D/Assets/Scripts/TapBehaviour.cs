31260b8a95710c7e869fd5cdc2d89c75cbda9162
Davide Zordan <davide.zordan@microsoft.com>
2017-07-30 16:29:06 +0100

    Using IInputClickHandler

--- MixedReality2D3D/Assets/Scripts/TapBehaviour.cs
+++ MixedReality2D3D/Assets/Scripts/TapBehaviour.cs
@@ -1,28 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
 using UnityEngine.VR.WSA.Input;
 
-public class TapBehaviour : MonoBehaviour {
-
-    GestureRecognizer _gr;
+public class TapBehaviour : MonoBehaviour, IInputClickHandler {
 
 	// Use this for initialization
 	void Start () {
-        _gr = new GestureRecognizer();
-        _gr.TappedEvent += _gr_TappedEvent;
-        _gr.StartCapturingGestures();
-	}
 
-    private void _gr_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
-    {
-        var contentPage = AppViewManager.Views["ContentPage"];
-        if (contentPage != null)
-        {
-           contentPage.SwitchAndConsolidate(contentPage);
-        }
-    }
+	}
 
     // Update is called once per frame
     void Update () {
 		
 	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var contentPage = AppViewManager.Views["ContentPage"];
+        if (contentPage != null)
+        {
+            contentPage.SwitchAndConsolidate(contentPage);
+        }
+    }
 }
