e4f7cd92b5e81a7ec0496be1f914b410e8f4f951
Davide Zordan <mail@davide.dev>
2020-01-24 22:51:03 +0000

    Added basic sample with Unity XR Interaction toolkit

---
+++ Unity XR Interaction Toolkit/Assets/Scripts/DebugManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Dev.Davide.UnityHelpers {
+
+	public class DebugManager {
+		public static bool isDebugging = false;
+
+		public static void Info(string message) {
+			if(!isDebugging) {
+				return;
+			} else {
+				Debug.Log(message);
+			}
+		}
+	}
+	
+}
